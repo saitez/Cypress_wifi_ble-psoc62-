7ad8b592467c2c0fbd0f87827e2ffad85120842f
saitez <vellankisaiteja114@gmail.com>
2020-03-14 23:24:09 +0530

    header file changed

--- tez_bmi160.h
+++ tez_bmi160.h
@@ -27,5 +27,128 @@ int8_t bmi160_init(struct bmi160_dev *dev);
 
 int8_t bmi160_get_regs(uint8_t reg_addr, uint8_t *data, uint16_t len, const struct struct bmi160_dev *dev );
 
+//writes the given data to the register address
+
+int8_t bmi160_set_regs(uint8_t reg_addr, uin8_t *data, uint16_t len, const struct bmi160_dev *dev );
+
+// soft reset
+
+int8_t bmi160_soft_reset(struct bmi160_dev *dev);
+
+// power mode , range, bandwidth
+
+int8_t bmi160_set_sens_conf(struct bmi160_dev *dev);
+
+
+// set power mode of the sensor
+
+int8_t bmi160_set_power_mode(struct bmi160_dev *dev);
+
+
+//get power mode of the sensor
+
+int8_t bmi160_get_power_mode(struct bmi160_dev *dev);
+
+
+//get sensor data
+
+int8_t bmi160_get_sensor_data(uint8_t select_sensor, struct bmi160_sensor_data *accel,struct bmi160_sensor_data *gyro,const struct bmi160_dev *dev );
+
+//sets necessary interrupt based on the user setting..
+
+int8_t bmi160_set_int_config(struct bmi160_int_settg *int_config,struct bmi160_dev *dev);
+
+// step count enable /disable
+
+int8_t bmi160_set_step_counter(uint8_t step_cnt_enable, const struct bmi160_dev *dev );
+
+//reads step counter value
+
+int8_t bmi160_read_step_counter(uint16_t *step_val, const struct bmi160_dev *dev);
+
+// auxilary read
+
+int8_t bmi160_aux_read(uint8_t reg_addr, uint8_t *aux_data, uint16_t len,const struct bmi160_dev *dev );
+
+// auxilary write
+
+int8_t bmi160_aux_write(uint8_t reg_addr, uint8_t *aux_data, uint16_t len, const struct bmi160_dev *dev);
+
+
+// aux init
+
+int8_t bmi160_aux_init(const struct bmi160_dev *dev);
+
+// aux sensor in auto mode
+
+int8_t bmi160_set_aux_auto_mode(uint8_t *data_addr, struct bmi160_dev *dev);
+
+//auxilary sensor manual enable or disable
+
+int8_t bmi160_config_aux_mode(const struct bmi160_dev *dev);
+
+//reads aux data
+
+int8_t bmi160_read_aux_data_auto_mode(uint8_t *aux_data, const struct bmi160_dev *dev);
+
+
+//self test
+
+int8_t bmi160_perform_self_test(uint8_t select_sensor, struct bmi160_dev  *dev );
+
+//get fifo data
+
+int8_t bmi160_get_fifo_data(struct bmi160_dev const *dev );
+
+//set fifo flush
+
+int8_t bmi160_set_fifo_flush(const struct bmi160_dev *dev);
+
+//set fifo config
+
+int8_t bmi160_set_fifo_config(uint8_t config, uint8_t enable, struct bmi160_dev const *dev);
+
+// set fifo down
+
+int8_t bmi160_set_fifo_down(uint8_t fifo_down, const struct bmi160_dev *dev);
+
+// set fifo water mark
+
+int8_t bmi160_set_fifo_wm(uint8_t fifo_wm, const struct bmi160_dev *dev);
+
+// extract the accelertometer fifo frames
+
+int8_t bmi160_extract_accel(struct bmi160_sensor_data *accel_data, uint8_t accel_length, struct bmi160_dev const *dev);
+
+
+
+// extract the gyro fifo frames
+
+int8_t bmi160_extract_accel(struct bmi160_sensor_data *gyro_data, uint8_t gyro_length, struct bmi160_dev const *dev);
+
+
+// extract the aux fifo frames
+
+int8_t bmi160_extract_accel(struct bmi160_sensor_data *aux_data, uint8_t aux_len, struct bmi160_dev const *dev);
+
+
+//start offset compensation
+
+int8_t bmi160_start_foc()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
  
